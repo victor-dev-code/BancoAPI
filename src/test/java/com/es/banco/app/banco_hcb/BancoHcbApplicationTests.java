9d799162b65f98adfadcb3bc83c1d37b8d7dac6f
Victor Hdez <hdezyvictor99@gmail.com>
2025-11-20 16:41:47 -0600

    Proyecto principal sin modificar

---
+++ src/test/java/com/es/banco/app/banco_hcb/BancoHcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.banco.app.banco_hcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoHcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
